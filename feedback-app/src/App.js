dcd3df5cc423f346cf36859aeabe4ad9c0a95a01
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-03 16:15:02 -0600

    feedback header

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
-      <h1>doggy</h1>
+      <Header />
+      <div className="container">
+        <h1>app</h1>
+      </div>
     </>
   )
 }
---
+++ notes/notes.md
@@ -0,0 +1,7 @@
+components can end in .js or .jsx
+
+ES6 snippets:
+
+"rfce" makes and exports a functional component, "\_rfce" does the same without importing React from "react"
+
+"impt" import proptypes
