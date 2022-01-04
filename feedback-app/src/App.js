d670fad32b25e1eba92425c1bb466bc146ab84ed
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-03 19:21:48 -0600

    about page added with react router dom

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { v4 as uuidv4 } from "uuid"
 import feedbackData from "./data/feedbackData"
 import Header from "./components/Header"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackList from "./components/FeedbackList"
+import AboutPage from "./pages/AboutPage"
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData)
@@ -22,12 +24,28 @@ function App() {
 
   return (
     <>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
