626c1c2ebcf6ee6999dc374e3cd6e1e5e523af16
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-04 20:58:39 -0600

    can now add feedback items to mock database through post request

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react"
-import { v4 as uuidv4 } from "uuid"
 
 const FeedbackContext = createContext()
 
@@ -21,9 +20,18 @@ export const FeedbackProvider = ({ children }) => {
     setIsLoading(false)
   }
 
-  const addFeedback = newFeedback => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async newFeedback => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
   const deleteFeedback = id => {
