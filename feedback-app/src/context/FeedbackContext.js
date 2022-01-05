e2dba1138bd48463d89c7d8f4e8f11d52ab5e538
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-04 21:04:02 -0600

    update and delete now persist to mock database

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -34,17 +34,29 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([data, ...feedback])
   }
 
-  const deleteFeedback = id => {
+  const deleteFeedback = async id => {
     if (window.confirm("Are you sure you want to delete this item?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" })
+
       setFeedback(feedback.filter(item => item.id !== id))
       setFeedbackEdit({ item: {}, edit: false })
     }
   }
 
   // update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json()
+
     setFeedback(
-      feedback.map(item => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map(item => (item.id === id ? { ...item, ...data } : item))
     )
     setFeedbackEdit({ item: {}, edit: false })
   }
