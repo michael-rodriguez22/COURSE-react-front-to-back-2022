37db8cafc6b8e13d532bddde34d976248b11797d
Michael Rodriguez <mikeyrod22@gmail.com>
2022-01-11 15:31:47 -0600

    refactor context to manipulate state through useReducer instead of useState

--- github-finder/src/context/github/GitHubContext.js
+++ github-finder/src/context/github/GitHubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react"
+import { createContext, useReducer } from "react"
+import githubReducer from "./GitHubReducer"
 
 const GitHubContext = createContext()
 
@@ -6,8 +7,12 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const TOKEN = process.env.REACT_APP_TOKEN
 
 export const GitHubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const initialState = {
+    users: [],
+    loading: true,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -17,15 +22,18 @@ export const GitHubProvider = ({ children }) => {
     })
 
     const data = await response.json()
-    setUsers(data)
-    setLoading(false)
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    })
   }
 
   return (
     <GitHubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
