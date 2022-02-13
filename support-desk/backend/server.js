65e9b56678966dd97912ae4b49b0b23f639d35e2
Michael Rodriguez <mikeyrod22@gmail.com>
2022-02-13 03:00:38 -0600

    configure heroku deployment

--- support-desk/backend/server.js
+++ support-desk/backend/server.js
@@ -1,5 +1,6 @@
 require("colors")
 require("dotenv").config()
+const path = require("path")
 const express = require("express")
 
 const app = express()
@@ -7,6 +8,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(require("./routes"))
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../", "frontend", "build", "index.html"))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" })
+  })
+}
 
 const { errorHandler } = require("./middleware")
 app.use(errorHandler)
